8c0fc81b3bebc33db4be96f9326b0f5bc590f078
Serhii Kaminskyi <kaminsky_s_s@ukr.net>
2025-05-19 01:10:46 +0300

    fix pTag

--- organick/src/components/utils/ptag.tsx
+++ organick/src/components/utils/ptag.tsx
@@ -5,6 +5,6 @@ interface IPTag {
     className?: string
 }
 
-export const PTag = ({ children, className }: IPTag): JSX.Element => {
+export const PTag = ({ children, className }: IPTag) => {
     return <p className={cn("font-opensans text-[18px] leading-8 text-[#525C60]", className)}>{children}</p>;
 }
